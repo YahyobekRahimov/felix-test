275c037879108a001bfa49733ea336f44351e963
Yahyobek Rahimov <141851971+YahyobekRahimov@users.noreply.github.com>
2024-03-30 00:09:51 +0500

    some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import {
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
 import "./App.css";
+import Login from "./pages/Login/Login";
+import SignUp from "./pages/SignUp/SignUp";
+import Home from "./pages/Home/Home";
 
 function App() {
-  return <div>App</div>;
+  const router = createBrowserRouter([
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <SignUp />,
+    },
+    {
+      path: "/",
+      element: <Home />,
+    },
+  ]);
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
